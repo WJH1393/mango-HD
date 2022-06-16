77f596ace91ed7537a4921fbc47df6fa0f723b41
WJH1393 <35983954+WJH1393@users.noreply.github.com>
2022-06-16 17:34:55 +0800

    整合swagger

--- mango/src/main/java/com/louis/mango/controller/TestMango.java
+++ mango/src/main/java/com/louis/mango/controller/TestMango.java
@@ -1,12 +1,13 @@
 package com.louis.mango.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestMango {
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String test(){
 
         return "基本服务搭建成功！";
