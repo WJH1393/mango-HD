4620d225cfafd0170e9f4e27cae05a41e307d7e7
WJH1393 <35983954+WJH1393@users.noreply.github.com>
2022-06-17 10:51:45 +0800

    整合mybatis

--- mango/src/main/java/com/louis/mango/controller/TestMango.java
+++ mango/src/main/java/com/louis/mango/controller/TestMango.java
@@ -1,16 +1,24 @@
 package com.louis.mango.controller;
 
+import com.louis.mango.model.SysUser;
+import com.louis.mango.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestMango {
 
+    @Autowired
+    private SysUserService service;
+
     @GetMapping("/test")
     public String test(){
-
-        return "基本服务搭建成功！";
+        List<SysUser> result = service.findAll();
+        return result.toString();
 
     }
 
