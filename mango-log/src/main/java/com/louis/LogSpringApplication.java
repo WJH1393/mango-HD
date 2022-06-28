b554152910c2019458de8888a9b5be4a897a0e13
WJH1393 <35983954+WJH1393@users.noreply.github.com>
2022-06-28 17:39:58 +0800

    整合日志

---
+++ mango-log/src/main/java/com/louis/LogSpringApplication.java
@@ -0,0 +1,11 @@
+package com.louis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogSpringApplication.class);
+    }
+}
