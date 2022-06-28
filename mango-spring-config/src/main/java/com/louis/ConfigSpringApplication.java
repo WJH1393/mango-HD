ede271af1b72ab9c9d26c498d18e5c94c7476ddf
WJH1393 <35983954+WJH1393@users.noreply.github.com>
2022-06-28 14:44:10 +0800

    提交分布式配置中心

---
+++ mango-spring-config/src/main/java/com/louis/ConfigSpringApplication.java
@@ -0,0 +1,16 @@
+package com.louis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigSpringApplication.class,args);
+    }
+}
