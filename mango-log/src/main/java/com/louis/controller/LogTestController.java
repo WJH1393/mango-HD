90e1cfbc4b7d6d6f51cbc073a0f5c65eed1c8255
WJH1393 <35983954+WJH1393@users.noreply.github.com>
2022-06-29 10:35:14 +0800

    整合日志，输出日志到具体目录

--- mango-log/src/main/java/com/louis/controller/LogTestController.java
+++ mango-log/src/main/java/com/louis/controller/LogTestController.java
@@ -1,12 +1,15 @@
 package com.louis.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LogTestController {
     @RequestMapping("/testLog")
     public String testLog(){
+        log.info("日志测试66666");
         return "this is my log test !";
     }
 }
