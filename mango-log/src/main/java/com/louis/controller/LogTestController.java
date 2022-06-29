e98664144fa517339ed2b4a59db8d971ee2b0212
WJH1393 <35983954+WJH1393@users.noreply.github.com>
2022-06-29 14:33:44 +0800

    修改日志输出

--- mango-log/src/main/java/com/louis/controller/LogTestController.java
+++ mango-log/src/main/java/com/louis/controller/LogTestController.java
@@ -9,7 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogTestController {
     @RequestMapping("/testLog")
     public String testLog(){
-        log.info("日志测试66666");
+        log.info("info日志测试66666");
+        log.error("error错误日志打印测试！");
+        log.trace("trace日志打印测试！");
+        log.warn("warn日志打印测试！");
+        log.debug("debugger日志打印测试");
         return "this is my log test !";
     }
 }
